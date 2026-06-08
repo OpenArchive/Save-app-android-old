ad8ec8b7dab32735e6a01eb2e70d196434aecca7
Prathieshna Vekneswaran <vekneswaran.prathieshna_adl@myboostback.co>
2026-06-08 10:02:51 +0530

    Bump version to 4.0.5 (30043).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,8 +67,8 @@ android {
         applicationId = "net.opendasharchive.openarchive"
         minSdk = 29
         targetSdk = 36
-        versionCode = 30042
-        versionName = "4.0.6"
+        versionCode = 30043
+        versionName = "4.0.5"
         multiDexEnabled = true
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
